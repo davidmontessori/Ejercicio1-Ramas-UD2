e70af54538f55ee73d2dc46266b61583eeb3ecb9
David Navarro Sanchez <22.mktt.davidnavarro@fundacionmontessori.com>
2024-11-21 17:19:34 +0100

    Saludos Commit

--- saludos/src/saludos/Saludos.java
+++ saludos/src/saludos/Saludos.java
@@ -1,9 +1,29 @@
 package saludos;
 
+import java.util.Scanner;
+
 public class Saludos {
+	final static int HORA = 4;
 
 	public static void main(String[] args) {
-	System.out.println("Hola Mundo");
+		System.out.println("Hola Mundo");
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("nombre y apellidos");
+		String nombreapellidos = input.nextLine();
+		System.out.println("Hola " + nombreapellidos);
+
+		if (HORA >= 6 && HORA < 12) {
+			System.out.println("¡Buenos días!");
+		} else if (HORA >= 12 && HORA < 18) {
+			System.out.println("¡Buenas tardes!");
+		} else if (HORA >= 18 && HORA < 24) {
+			System.out.println("¡Buenas noches!");
+		} else {
+			System.out.println("¡Hola, es madrugada!");
+		}
+
+		input.close();
 	}
 
 }
